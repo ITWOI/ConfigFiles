4b406315428fdf710574fa7c84ec7a8cf7f3abe9
WY <fff000147369@gmail.com>
2017-03-14 19:55:01 -0700

    add ycm

--- autoinstall.sh
+++ autoinstall.sh
@@ -5,3 +5,10 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 git clone --depth=1 https://github.com/tmux-plugins/tmux-resurrect
 cp .tmux.conf ~/.tmux.conf
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/.vimrc
+
+# open vim -> :BundleInstall
+# cd ~/.vim/bundle/YouCompleteMe
+# ./install.py --clang-completer
