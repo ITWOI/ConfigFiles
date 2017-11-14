eda234dc3e3762a8b9737453c05fb6ae40fac5ca
ITWOI <fff000147369@gmail.com>
2017-11-14 14:21:25 -0800

    recent update

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,4 +1,4 @@
-sudo apt-get install vim tmux zsh -y
+sudo apt-get install vim tmux zsh curl -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp .zshrc ~/.zshrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
@@ -8,10 +8,10 @@ cp .tmux.conf ~/.tmux.conf
 cp .vimrc ~/.vimrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-cd ~
+#git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+#cd ~/.vim/bundle/YouCompleteMe
+#git submodule update --init --recursive
+#cd ~
 
 # open vim -> :BundleInstall
 # cd ~/.vim/bundle/YouCompleteMe
