231eb59972071a72a1abc23ece077289a0ce1a6a
ITWOI <fff000147369@gmail.com>
2017-11-26 10:53:41 -0800

    fix a bug

--- autoinstall.sh
+++ autoinstall.sh
@@ -5,6 +5,7 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 git clone --depth=1 https://github.com/tmux-plugins/tmux-resurrect
 cp .tmux.conf ~/.tmux.conf
+cp .tmux.conf.local ~/.tmux.conf.local
 cp .vimrc ~/.vimrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
